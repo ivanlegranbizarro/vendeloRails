b4bc39436ea74031b3f03407d04f8bef81c02a3e
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-13 16:00:00 +0100

    image  products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,6 +46,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :price, :description)
+    params.require(:product).permit(:title, :price, :description, :image)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_one_attached :image
   validates :title, presence: true, uniqueness: { scope: :id }, length: { minimum: 3, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 500 }
   validates :price, presence: true, numericality: { greater_than: 0, less_than: 100000 }
