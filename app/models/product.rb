ec1db29ed8871c8cb62cf2325b57afd5bcc637fe
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-13 07:56:11 +0100

    product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, presence: true, uniqueness: true, length: { minimum: 3, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 500 }
+  validates :price, presence: true, numericality: { greater_than: 0, less_than: 100000 }
 end
