579cd295c6b68ca4ea64b6610bbb4212da9084de
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-13 17:42:24 +0100

    traducir web inglés-español

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,10 @@ module Vendelo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # available languages
+    config.i18n.available_locales = [:en, :es]
+
+    # default language
+    config.i18n.default_locale = :es
   end
 end
