ce03d6b5f3973a4d93ea812ff983fbef5a6eab3c
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-13 11:36:08 +0100

    botón de editar producto

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  validates :title, presence: true, uniqueness: true, length: { minimum: 3, maximum: 100 }
+  validates :title, presence: true, uniqueness: { scope: :id }, length: { minimum: 3, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 500 }
   validates :price, presence: true, numericality: { greater_than: 0, less_than: 100000 }
 end
