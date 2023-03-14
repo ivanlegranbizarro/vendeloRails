469522f212b67809d63c8708780dc402f1de317e
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-14 08:32:42 +0100

    adjusting category controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,63 +1,51 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[edit update destroy]
 
-  # GET /categories or /categories.json
   def index
     @categories = Category.all
   end
 
-  # GET /categories/new
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
-  def edit; end
-
-  # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
-
-    respond_to do |format|
-      if @category.save
-        flash[:notice] = t('.created')
-        format.html { redirect_to categories_url }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @category.save
+      flash[:notice] = t('.created')
+      redirect_to categories_url
+    else
+      render :new, status: :unprocessable_entity, alert: t('.not_created')
     end
   end
 
-  # PATCH/PUT /categories/1 or /categories/1.json
+  def edit; end
+
   def update
-    respond_to do |format|
-      if @category.update(category_params)
-        flash[:notice] = t('.updated')
-        format.html { redirect_to categories_url }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
+    if @category.update(category_params)
+      flash[:notice] = t('.updated')
+      redirect_to categories_url
+    else
+      render :edit, status: :unprocessable_entity, alert: t('.not_updated')
     end
   end
 
-  # DELETE /categories/1 or /categories/1.json
   def destroy
     @category.destroy
-
-    respond_to do |format|
-      flash[:notice] = t('.destroyed')
-      format.html { redirect_to categories_url }
+    if @category.destroyed?
+      flash[:notice] = t('.deleted')
+      redirect_to categories_url
+    else
+      redirect_to categories_url, status: :unprocessable_entity, alert: t('.not_deleted')
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_category
     @category = Category.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def category_params
     params.require(:category).permit(:name)
   end
